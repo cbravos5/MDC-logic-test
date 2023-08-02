a34d947444312c4d004c81187998d27632e95112
cbravos <cnbravos5@gmail.com>
2023-08-02 09:38:29 -0300

    add interest calculator solution

---
+++ 8-interestCalculation.c
@@ -0,0 +1,40 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void getValues(float *capital, float *rate, float *time)
+{
+  printf("Type the investment initial capital: \n");
+  scanf("%f", capital);
+
+  printf("Type the investment rate in percentage: \n");
+  scanf("%f", rate);
+
+  printf("Type the investment time in months : \n");
+  scanf("%f", time);
+}
+
+float getSimpleInterest(float capital, float rate, float time)
+{
+  return capital + (capital * rate * time / 100);
+}
+
+float getCompoundInterest(float capital, float rate, float time)
+{
+  return capital * pow((1 + (rate / 100)), time);
+}
+
+int main()
+{
+  float capital, rate, time, simpleResult, compoundResult;
+
+  getValues(&capital, &rate, &time);
+
+  simpleResult = getSimpleInterest(capital, rate, time);
+  compoundResult = getCompoundInterest(capital, rate, time);
+
+  printf("Your final investment value using simple interest rate is %.3f\n", simpleResult);
+  printf("Your final investment value using compound interest rate is %.3f\n", compoundResult);
+
+  return 0;
+}
